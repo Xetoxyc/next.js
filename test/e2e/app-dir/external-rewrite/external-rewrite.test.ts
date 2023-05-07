3e0993aae9294aefeb93d6edd8943f647a77c16e
Tobias Sittenauer <tobias.sittenauer@xpecto.com>
2023-05-08 00:22:33 +0200

    remove get test because of rate limiting

--- test/e2e/app-dir/external-rewrite/external-rewrite.test.ts
+++ test/e2e/app-dir/external-rewrite/external-rewrite.test.ts
@@ -6,11 +6,6 @@ createNextDescribe(
     files: __dirname,
   },
   ({ next }) => {
-    it('should work when using GET to an external url', async () => {
-      const res = await next.fetch('/test')
-      const json = await res.json()
-      expect(json.quickstart).toBe('https://pipedream.com/quickstart/')
-    })
     it('should work when using POST to an external url', async () => {
       const res = await next.fetch('/test', {
         method: 'POST',
